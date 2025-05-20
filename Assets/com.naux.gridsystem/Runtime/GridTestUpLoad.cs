df2b8cace9505700ca02ff522421a74708651d2f
Naux <naux.naoh@gmail.com>
2025-05-21 03:53:05 +0700

    add asem b li

--- Assets/com.naux.gridsystem/Runtime/GridTestUpLoad.cs
+++ Assets/com.naux.gridsystem/Runtime/GridTestUpLoad.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class GridTestUpLoad : MonoBehaviour
+namespace N.Test
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class GridTestUpLoad : MonoBehaviour
     {
-        
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
